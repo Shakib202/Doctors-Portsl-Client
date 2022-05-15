bfd09514c56a02b0f54aafc0dfd4992e90a4ece1
Shakib202 <shakib2020360@gmail.com>
2022-05-16 02:27:31 +0600

    added service

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,22 +1,18 @@
 import React from "react";
-import chair from '../../assets/images/chair.png';
+import chair from "../../assets/images/chair.png";
 
 const Banner = () => {
   return (
     <div class="hero min-h-screen">
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={chair}
-          class="max-w-sm rounded-lg shadow-2xl"
-        />
+        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            A doctor is someone who is experienced and certified to practice
+            medicine to help maintain or restore physical and mental health.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
         </div>
       </div>
     </div>
