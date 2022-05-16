94e75ed67f735963319a7f3590f33f6e2192800f
Shakib202 <shakib2020360@gmail.com>
2022-05-17 02:44:36 +0600

    modal add done

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,19 +1,30 @@
 import React from "react";
 
-const Service = ({ service }) => {
-  const {name, slots} = service;
+const Service = ({ service, setTreatment }) => {
+  const { name, slots } = service;
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
       <div class="card-body">
         <h2 class="card-title text-secondary">{name}</h2>
-        <p>{
-          slots.length > 0
-          ? <span>{slots[0]}</span>
-          : <span className="text-primary">Try another date.</span>
-          }</p>
-        <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+        <p>
+          {slots.length > 0 ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-primary">Try another date.</span>
+          )}
+        </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
         <div class="card-actions justify-center">
-          <button disabled={slots.length === 0} class="btn btn-secondary text-white uppercase">Buy Appointment</button>
+          <label
+            for="booking-modal"
+            disabled={slots.length === 0}
+            onClick={setTreatment(service)}
+            class="btn btn-secondary text-white uppercase"
+          >
+            Buy Appointment
+          </label>
         </div>
       </div>
     </div>
