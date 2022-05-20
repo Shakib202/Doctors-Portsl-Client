e25219f27c868fb7b73b8ec5bde4b6c1c28f2fbf
Shakib202 <shakib2020360@gmail.com>
2022-05-21 01:29:56 +0600

    post data from ui to backend

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,11 +2,12 @@ import React from "react";
 import { format } from "date-fns";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { toast } from "react-toastify";
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
-  const formattedDate = format(date, 'PP');
+  const formattedDate = format(date, "PP");
   const handleBooking = (event) => {
     event.preventDefault();
     const slot = event.target.slot.value;
@@ -16,14 +17,46 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
       treatment: name,
       date: formattedDate,
       slot,
-      patient:user.email,
-      patientName:user.displayName,
+      patient: user.email,
+      patientName: user.displayName,
       phone: event.target.phone.value,
-    }
-    
+    };
 
-    // to close the modal
-    setTreatment(null);
+    /*
+    // post data from ui to backend
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setTreatment(null);
+      });
+    */
+
+    // post data from ui to backend
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          toast(`Appointment is set, ${formattedDate} at ${slot}`);
+        } else {
+          toast.error(
+            `Already have an appointment, ${data.booking?.date} at ${data.booking?.slot}`
+          );
+        }
+        setTreatment(null);
+      });
   };
 
   return (
@@ -32,7 +65,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <label
-           htmlFor="booking-modal"
+            htmlFor="booking-modal"
             className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
@@ -55,21 +88,23 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
               className="select select-bordered w-full max-w-xs"
             >
               {slots.map((slot, index) => (
-                <option key={index} value={slot}>{slot}</option>
+                <option key={index} value={slot}>
+                  {slot}
+                </option>
               ))}
             </select>
             <input
               type="text"
               name="name"
               disabled
-              value={user?.displayName || ''}
+              value={user?.displayName || ""}
               className="input input-bordered w-full max-w-xs"
             />
             <input
               type="email"
               name="email"
               disabled
-              value={user?.email || ''}
+              value={user?.email || ""}
               className="input input-bordered w-full max-w-xs"
             />
             <input
