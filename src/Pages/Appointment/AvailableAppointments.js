32a7480ec2106b131749bfd2d265427d02e8f22c
Shakib202 <shakib2020360@gmail.com>
2022-05-20 22:29:54 +0600

    fix some warning

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import BookingModal from "./BookingModal";
 import Service from "./Service";
 
@@ -14,11 +14,11 @@ const AvailableAppointments = ({ date }) => {
   }, []);
 
   return (
-    <div>
-      <h4 className="text-xl text-secondary font-bold text-center">
+    <div className="my-10">
+      <h4 className="text-xl text-secondary text-center my-12">
         Available Appointments on {format(date, "PP")}
       </h4>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {services.map((service) => (
           <Service
             key={service._id}
@@ -29,9 +29,9 @@ const AvailableAppointments = ({ date }) => {
       </div>
       {treatment && (
         <BookingModal
-          setTreatment={setTreatment}
           date={date}
           treatment={treatment}
+          setTreatment={setTreatment}
         ></BookingModal>
       )}
     </div>
@@ -39,3 +39,44 @@ const AvailableAppointments = ({ date }) => {
 };
 
 export default AvailableAppointments;
+// import { format } from "date-fns";
+// import React, { useEffect, useState } from "react";
+// import BookingModal from "./BookingModal";
+// import Service from "./Service";
+
+// const AvailableAppointments = ({ date }) => {
+//   const [services, setServices] = useState([]);
+//   const [treatment, setTreatment] = useState(null);
+
+//   useEffect(() => {
+//     fetch("http://localhost:5000/service")
+//       .then((res) => res.json())
+//       .then((data) => setServices(data));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h4 className="text-xl text-secondary font-bold text-center">
+//         Available Appointments on {format(date, "PP")}
+//       </h4>
+//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+//         {services.map((service) => (
+//           <Service
+//             key={service._id}
+//             service={service}
+//             setTreatment={setTreatment}
+//           ></Service>
+//         ))}
+//       </div>
+//       {treatment && (
+//         <BookingModal
+//           setTreatment={setTreatment}
+//           date={date}
+//           treatment={treatment}
+//         ></BookingModal>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AvailableAppointments;
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,13 +4,13 @@ const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-      <div className="card-body text-center my-10">
+      <div className="card-body text-center">
         <h2 className="text-xl font-bold text-secondary">{name}</h2>
         <p>
           {slots.length > 0 ? (
             <span>{slots[0]}</span>
           ) : (
-            <span className="text-primary">Try another date.</span>
+            <span className="text-yellow-500">Try another date.</span>
           )}
         </p>
         <p>
@@ -20,10 +20,10 @@ const Service = ({ service, setTreatment }) => {
           <label
             htmlFor="booking-modal"
             disabled={slots.length === 0}
-            onClick={setTreatment(service)}
+            onClick={() => setTreatment(service)}
             className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
           >
-            Buy Appointment
+            Book Appointment
           </label>
         </div>
       </div>
@@ -32,3 +32,38 @@ const Service = ({ service, setTreatment }) => {
 };
 
 export default Service;
+
+// import React from "react";
+
+// const Service = ({ service, setTreatment }) => {
+//   const { name, slots } = service;
+//   return (
+//     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+//       <div className="card-body text-center my-10">
+//         <h2 className="text-xl font-bold text-secondary">{name}</h2>
+//         <p>
+//           {slots.length > 0 ? (
+//             <span>{slots[0]}</span>
+//           ) : (
+//             <span className="text-primary">Try another date.</span>
+//           )}
+//         </p>
+//         <p>
+//           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+//         </p>
+//         <div className="card-actions justify-center">
+//           <label
+//             htmlFor="booking-modal"
+//             disabled={slots.length === 0}
+//             onClick={setTreatment(service)}
+//             className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
+//           >
+//             Buy Appointment
+//           </label>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Service;
